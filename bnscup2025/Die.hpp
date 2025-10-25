6ccf8b6ab7342f2d499cca6877a1d98e3f2d9c1f
Tanorenn <kuronosu102938@gmail.com>
2025-10-25 12:36:40 +0900

    ダイスの名前、レア度、コスト、説明、TextureAsset用のキーを追加

--- bnscup2025/Die.hpp
+++ bnscup2025/Die.hpp
@@ -9,6 +9,13 @@ enum class RollOrder {
 	FINAL = 30
 };
 
+enum class Rarity {
+	Common,
+	Rare,
+	Epic,
+	Legendary
+};
+
 using RollFunc = std::function<int(const Die&, const Array<Die>&)>;
 using DrawFunc = std::function<void(const Vec2&, const Die)>;
 
@@ -20,6 +27,13 @@ struct Die
 	RollOrder order = RollOrder::PRIMARY;
 	RollFunc rollFunc;
 	DrawFunc drawFunc;
+
+	String name = U"";
+	Rarity rarity = Rarity::Common;
+	int cost = 0;
+	String discription = U"";
+	String textureKey = U"";
+
 	bool locked = false;
 	void roll(const Array<Die>& dices)
 	{
@@ -53,7 +67,12 @@ namespace Dice{
 			faceRect.rounded(3).draw((self.locked || !self.value) ? ColorF{0.6} : ColorF{1.0});
 			faceRect.rounded(3).drawFrame(1, ColorF{ 0 });
 			if (self.value)FontAsset(U"Bold")(Format(self.value.value())).drawAt(faceRect.center(), ColorF{ 0.1 });
-		}
+		},
+		U"普通のダイス",
+		Rarity::Common,
+		20,
+		U"普通のダイス。1～6までの面が揃っている。",
+		U"StandardDice"
 	};
 
 	/// @brief 偶数ダイス
@@ -71,7 +90,12 @@ namespace Dice{
 			faceRect.rounded(3).draw((self.locked || !self.value) ? HSV(180, 0.12, 0.8) : HSV(180, 0.12, 1));
 			faceRect.rounded(3).drawFrame(1, ColorF{ 0 });
 			if (self.value)FontAsset(U"Bold")(Format(self.value.value())).drawAt(faceRect.center(), ColorF{ 0.1 });
-		}
+		},
+		U"偶数ダイス",
+		Rarity::Common,
+		50,
+		U"偶数のみで構成されたダイス。普通のダイスより出目がちょっと高い。",
+		U"EvenDice"
 	};
 
 	/// @brief 奇数ダイス
@@ -89,7 +113,12 @@ namespace Dice{
 			faceRect.rounded(3).draw((self.locked || !self.value) ? HSV(0, 0.12, 0.8) : HSV(0, 0.12, 1));
 			faceRect.rounded(3).drawFrame(1, ColorF{ 0 });
 			if (self.value)FontAsset(U"Bold")(Format(self.value.value())).drawAt(faceRect.center(), ColorF{ 0.1 });
-		}
+		},
+		U"奇数ダイス",
+		Rarity::Common,
+		50,
+		U"奇数のみで構成されたダイス。普通のダイスより出目がちょっと低い。",
+		U"OddDice"
 	};
 
 	/// @brief コインダイス 二分の一の確率で1か6が出る
@@ -107,7 +136,12 @@ namespace Dice{
 			faceCircle.draw((self.locked || !self.value) ? HSV(54, 0.77, 0.8) : HSV(54, 0.77, 1));
 			faceCircle.drawFrame(1, ColorF{ 0 });
 			if (self.value)FontAsset(U"Bold")(Format(self.value.value())).drawAt(faceCircle.center, ColorF{ 0.1 });
-		}
+		},
+		U"コイン",
+		Rarity::Common,
+		50,
+		U"二分の一の確率で1か6が出るコイン。",
+		U"Coin"
 	};
 
 	/// @brief コインダイス 二分の一の確率で1か8が出る
@@ -125,7 +159,12 @@ namespace Dice{
 			faceCircle.draw((self.locked || !self.value) ? HSV(38, 1, 0.8) : HSV(38, 1, 1));
 			faceCircle.drawFrame(1, ColorF{ 0 });
 			if (self.value)FontAsset(U"Bold")(Format(self.value.value())).drawAt(faceCircle.center, ColorF{ 0.1 });
-		}
+		},
+		U"ハイコイン",
+		Rarity::Rare,
+		100,
+		U"少し出目が高くなったコイン。一か八かの賭けに出る際に使用する。",
+		U"HighCoin"
 	};
 
 
